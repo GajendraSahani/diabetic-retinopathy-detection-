f8d204783476d55cf2d1e25526e9c1d799d5c2e5
Gajendra Sahani <gajendra.sahani@gmail.com>
2026-06-08 20:38:39 +0530

    Update config.py

--- core-engine/src/config.py
+++ core-engine/src/config.py
@@ -3,11 +3,13 @@ import tensorflow as tf
 
 class Config:
     SEED = 42
+    
     IMG_HEIGHT = 224
     IMG_WIDTH = 224
     IMG_SIZE = (IMG_HEIGHT, IMG_WIDTH)
     CHANNELS = 3
     NUM_CLASSES = 5
+    
     BATCH_SIZE = 32
     EPOCHS = 30
     LEARNING_RATE = 1e-4
